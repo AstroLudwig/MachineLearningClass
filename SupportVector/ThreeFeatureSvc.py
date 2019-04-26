78eba5a2714a42db5ef4ee107da865a1de4897f3
Bethany Ann Ludwig <Ludwig@astro.utoronto.ca>
2019-04-26 13:16:32 -0400

    Finish Support Vector. Notes on Clustering

---
+++ SupportVector/ThreeFeatureSvc.py
@@ -0,0 +1,112 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import KFold
+from sklearn.svm import SVC
+import random 
+from sklearn.model_selection import KFold
+from mpl_toolkits.mplot3d import Axes3D 
+
+def get_tests(clf_func,test_x,test_y,label):
+	
+	# Try out your predictions 
+	predict_y = clf_func.predict(test_x)
+
+	# Were your predictions true or false? 
+	true_labels = predict_y[predict_y == test_y]
+	false_labels = predict_y[predict_y != test_y]
+
+	"I think it's a blue star and it is a blue star."
+	TruePositive = len(true_labels[true_labels == label])
+
+	"I don't think it's a blue star, but it is a blue star."
+	FalseNegative = len(false_labels[false_labels != label])
+
+	"I think it's a blue star, but it's not a blue star."
+	FalsePositive = len(false_labels[false_labels == label])
+
+	Completeness = TruePositive / (TruePositive + FalseNegative)
+	Purity = TruePositive / (TruePositive + FalsePositive)
+
+	return Completeness, Purity 
+
+contamination = 3
+
+# Generate Random Test Data
+
+# Team 1, Blue Stars 
+
+blue_x = np.arange(.1, 3, .1) 
+blue_y = np.arange(10, 15, (15-10)/len(blue_x)) 
+blue_z = np.arange(4,9,(9-4)/len(blue_x))
+
+random.shuffle(blue_y); random.shuffle(blue_x)
+random.shuffle(blue_z)
+
+# Team 2, Black Diamonds
+
+black_x = np.arange(2.7, 6, .1) 
+black_y = np.arange(5, 12, (12-5)/len(black_x)) 
+black_z = np.arange(4.15,9,(9-4.15)/len(black_x))
+
+random.shuffle(black_y); random.shuffle(black_x)
+random.shuffle(black_z)
+ 
+
+data = np.zeros([len(blue_y)+len(black_y),3])
+data[:,0] = np.append(blue_x,black_x)
+data[:,1] = np.append(blue_y,black_y)
+data[:,2] = np.append(blue_z,black_z)
+
+labels = np.append(np.repeat("Blue",len(blue_y)),np.repeat("Black",len(black_y)))
+
+param_x,param_y,param_z = np.meshgrid(np.arange(0,7,.1),np.arange(3,16,.1),np.arange(3,10,.1),indexing="ij")
+
+param_data = np.c_[param_x.ravel(),param_y.ravel(),param_z.ravel()]
+
+kf = KFold(n_splits=2,shuffle=True)
+for train_index, test_index in kf.split(data[:,0]):
+	train_x, test_x = data[train_index], data[test_index]
+	train_y, test_y = labels[train_index], labels[test_index]
+
+
+# Linear Fit
+svc = SVC(C= contamination, kernel='linear')
+svc.fit(train_x,train_y)
+
+decision_func = svc.decision_function(param_data)
+
+
+comp_blue, pure_blue = get_tests(svc,data,labels,"Blue")
+comp_black, pure_black = get_tests(svc,data,labels,"Black")
+
+# 3D Plot
+plot3d = False 
+if plot3d: 
+	fig = plt.figure()
+	ax = fig.add_subplot(111,projection='3d')
+	ax.scatter(blue_x,blue_y,blue_z,marker="*",c="blue")
+	ax.scatter(black_x,black_y,black_z,marker="D",c="black")
+
+Z = decision_func.reshape(param_x.shape)
+
+f, (ax,bx) = plt.subplots(1,2)
+
+ax.scatter(blue_x,blue_y,marker="*",c="blue")
+ax.scatter(black_x,black_y,marker="D",c="black")
+ax.contour(param_x[:,:,0],param_y[:,:,0],np.mean(Z,axis=2),[-1,0,1])
+ax.set_xlabel("Parameter X"); ax.set_ylabel("Parameter Y")
+ax.scatter(svc.support_vectors_[:,0],svc.support_vectors_[:,1],edgecolors="red",marker="o",facecolors='none',s=120)
+
+bx.scatter(blue_x,blue_z,marker="*",c="blue")
+bx.scatter(black_x,black_z,marker="D",c="black")
+bx.contour(param_x[:,0,:],param_z[:,0,:],np.mean(Z,axis=1),[-1,0,1])
+
+bx.set_xlabel("Parameter X"); bx.set_ylabel("Parameter Z")
+bx.scatter(svc.support_vectors_[:,0],svc.support_vectors_[:,2],edgecolors="red",marker="o",facecolors='none',s=120)
+
+
+f.suptitle(("Levels: -1,0,1\n Contamination: {}, Kernel: Linear \n Blue Completess: {:.3f} Purity: {:.3f} \n Black Completess: {:.3f} Purity: {:.3f}").format(contamination,comp_blue, pure_blue,comp_black, pure_black))
+
+plt.show()
