0d3a4867e95f32f85ac7a7a4a454ef075d4dac6f
Bethany Ann Ludwig <Ludwig@astro.utoronto.ca>
2019-04-30 15:05:57 -0400

    Finish clustering assignment Include Hierarchical notes

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -25,34 +25,89 @@ realX[:,1] = u_ri
 # Fake Data
 n_samples = 1500
 random_state = 170
+num = 10
+nnum = 5
 
 X, y = make_blobs(n_samples=n_samples, random_state=random_state)
 
-def get_inertia(X,num):
-	KM = KMeans(n_clusters=num, random_state=random_state) 
+def get_inertia_predict(X,num):
+	KM = KMeans(n_clusters=num, random_state=random_state,init="random") 
 	y_pred = KM.fit_predict(X)
 	inertia = KM.inertia_ 
-
-	return inertia
+	return inertia, y_pred
 
 def get_ypred(X,num):
-	KM = KMeans(n_clusters=num, random_state=random_state) 
+	KM = KMeans(n_clusters=num, random_state=random_state,init="random") 
 	y_pred = KM.fit_predict(X)
 	inertia = KM.inertia_ 
 
-	return y_pred
+	return y_pred	
 
-inertia = [get_inertia(X,i) for i in range(1,10)]
+def get_inertia_range(X, num):
+	inertia = np.zeros(num +1)
+	for i in range(1,num+1):
+		print(i)
+		KM = KMeans(n_clusters=i, n_init=1, random_state=random_state,init="random").fit(X) 
+		inertia[i] = KM.inertia_ 
+	return inertia[1:]
 
+def get_init(X, n_cluster,num,init):
+	inertia = np.zeros(num +1)
+	for i in range(1,num+1):
+		print(i)
+		KM = KMeans(n_clusters=n_cluster, n_init=i, random_state=random_state,init=init).fit(X) 
+		inertia[i] = KM.inertia_ 
+	return inertia[1:]
+
+
+real_inertia = get_inertia_range(realX,num)
+sim_inertia = get_inertia_range(X,num)
+
+real_init_random = get_init(realX,2,nnum,"random")
+sim_init_random = get_init(X,3,nnum,"random")
+real_init_kmeans = get_init(realX,2,nnum,"k-means++")
+sim_init_kmeans = get_init(X,2,nnum,"k-means++")
 
 f, (ax,bx) = plt.subplots(1,2)
 ax.scatter(realX[:,0],realX[:,1],c=get_ypred(realX,2))
+ax.set_xlabel("g-r",size="large")
+ax.set_ylabel("r-i",size="large")
 bx.scatter(X[:, 0], X[:, 1],c=get_ypred(X,3))
+bx.set_xlabel("Parameter 1",size="large")
+bx.set_ylabel("Parameter 2",size="large")
+ax.set_title("Quasar Data",size="large")
+bx.set_title("Simulated Data",size="large")
+plt.savefig("ExampleData.png")
 
 
-plt.figure()
-plt.plot(range(1,10),inertia)
-plt.xlabel("Number of Clusters")
-plt.ylabel("Inertia")
-plt.title("Simulated Data")
+f, (ax,bx) = plt.subplots(1,2)
+ax.plot(real_inertia,range(1,num+1))
+ax.set_ylabel("Number of Clusters",size="large")
+ax.set_xlabel("Inertia",size="large")
+ax.set_title("Quasar Data",size="large")
+
+bx.plot(sim_inertia,range(1,num+1))
+bx.set_xlabel("Inertia",size="large")
+bx.set_title("Simulated Data",size="large")
+plt.savefig("NumberOfClusters.png")
+
+f, axes = plt.subplots(2,2)
+# n-init, vs inertia
+axes[0,0].plot(real_init_random,range(1,nnum+1))
+axes[0,1].plot(sim_init_random,range(1,nnum+1))
+
+axes[1,0].set_xlabel("inertia")
+axes[1,1].set_xlabel("inertia")
+
+axes[0,0].set_ylabel("n-init: random")
+axes[1,0].set_ylabel("n-init: k-means++")
+
+axes[0,0].set_title("Quasar Data",size="large")
+axes[0,1].set_title("Simulated Data",size="large")
+
+axes[1,0].plot(real_init_kmeans,range(1,nnum+1))
+axes[1,1].plot(sim_init_kmeans,range(1,nnum+1))
+plt.savefig("n-init.png")
+
 plt.show()
+
